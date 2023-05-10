d873464cc4334f39c9740c94e50dc325391ffe31
Jacob Torrey <jacob@thinkst.com>
2023-05-10 16:44:40 -0600

    Clean up test runner
    Signed-off-by: Jacob Torrey <jacob@thinkst.com>

--- test_lzma_detect.py
+++ test_lzma_detect.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import pytest, os
-from lzma_detect import run_on_file_chunked as run_on_file
+from lzma_detect import run_on_file_chunked
 
 AI_SAMPLE_DIR = 'samples/llm-generated/'
 HUMAN_SAMPLE_DIR = 'samples/human-generated/'
@@ -14,10 +14,10 @@ def test_training_file():
 
 @pytest.mark.parametrize('f', human_files)
 def test_human_samples(f):
-    (classification, score) = run_on_file(HUMAN_SAMPLE_DIR + f)
+    (classification, score) = run_on_file_chunked(HUMAN_SAMPLE_DIR + f)
     assert classification == 'Human', f + ' is a human-generated file, misclassified as AI-generated with confidence ' + str(round(score, 8))
 
 @pytest.mark.parametrize('f', ai_files)
 def test_llm_sample(f):
-    (classification, score) = run_on_file(AI_SAMPLE_DIR + f)
+    (classification, score) = run_on_file_chunked(AI_SAMPLE_DIR + f)
     assert classification == 'AI', f + ' is an LLM-generated file, misclassified as human-generated with confidence ' + str(round(score, 8))
