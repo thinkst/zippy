f00c860c05727ddd28e49aa4d818b3189e07e542
Jacob Torrey <jacob@thinkst.com>
2023-05-18 08:31:40 -0600

    Added GPT-2 and WebText datasets
    Signed-off-by: Jacob Torrey <jacob@thinkst.com>

--- test_lzma_detect.py
+++ test_lzma_detect.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import pytest, os
+import pytest, os, jsonlines
 from warnings import warn
-from lzma_detect import run_on_file_chunked, PRELUDE_STR, LzmaLlmDetector
+from lzma_detect import run_on_file_chunked, run_on_text_chunked, PRELUDE_STR, LzmaLlmDetector
 
 AI_SAMPLE_DIR = 'samples/llm-generated/'
 HUMAN_SAMPLE_DIR = 'samples/human-generated/'
@@ -39,3 +39,29 @@ def test_llm_sample(f):
            warn("Misclassified " + f + " with score of: " + str(round(score, 8)))
        else:
            warn("Unable to confidently classify: " + f)
+
+MIN_LEN = 150
+
+HUMAN_JSONL_FILE = 'samples/webtext.test.jsonl'
+human_samples = []
+with jsonlines.open(HUMAN_JSONL_FILE) as reader:
+    for obj in reader:
+        if obj.get('length', 0) >= MIN_LEN:
+            human_samples.append(obj)
+
+@pytest.mark.parametrize('i', human_samples[0:250])
+def test_human_jsonl(i):
+    (classification, score) = run_on_text_chunked(i.get('text', ''), fuzziness=FUZZINESS, prelude_ratio=PRELUDE_RATIO)
+    assert classification == 'Human', HUMAN_JSONL_FILE + ':' + str(i.get('id')) + ' (len: ' + str(i.get('length', -1)) + ') is a human-generated sample, misclassified as AI-generated with confidence ' + str(round(score, 8))
+
+AI_JSONL_FILE = 'samples/xl-1542M.test.jsonl'
+ai_samples = []
+with jsonlines.open(AI_JSONL_FILE) as reader:
+    for obj in reader:
+        if obj.get('length', 0) >= MIN_LEN:
+            ai_samples.append(obj)
+
+@pytest.mark.parametrize('i', ai_samples[0:250])
+def test_llm_jsonl(i):
+    (classification, score) = run_on_text_chunked(i.get('text', ''), fuzziness=FUZZINESS, prelude_ratio=PRELUDE_RATIO)
+    assert classification == 'AI', AI_JSONL_FILE + ':' + str(i.get('id')) + ' (text: ' + i.get('text', "").replace('\n', ' ')[:50] + ') is an LLM-generated sample, misclassified as human-generated with confidence ' + str(round(score, 8))
