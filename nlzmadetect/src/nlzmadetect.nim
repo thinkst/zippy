81bdb8e5ddc8709a802f9f90f3b531197b1d215b
Jacob Torrey <jacob@thinkst.com>
2023-09-25 20:08:52 -0600

    Added ZLib options for NimJS versions to experiment with
    Signed-off-by: Jacob Torrey <jacob@thinkst.com>

--- nlzmadetect/src/nlzmadetect.nim
+++ nlzmadetect/src/nlzmadetect.nim
@@ -1,43 +1,58 @@
 when defined(c):
-  import std/[re, threadpool, encodings]
+  import std/[re, encodings]
   import lzma
 when defined(js):
   import std/[jsffi, jsre]
-  import dom
-import std/math
+import std/[math, async]
 import strutils
 when isMainModule and defined(c):
   import std/[parseopt, os]
 
 when defined(js) and not defined(extension):
+  import dom
   var local_prelude : string = ""
 when defined(extension):
   var local_prelude {.exportc.} : cstring = ""
 
+type
+  Engine = enum
+    LZMAEngine, ZLibEngine
+
 var COMPRESSION_PRESET {.exportc.} = 2.int32
 const SHORT_SAMPLE_THRESHOLD = 350
 
+const ACTIVE_ENGINE = Engine.LZMAEngine
+
 const PRELUDE_FILE = "../../ai-generated.txt"
-const PRELUDE_STR = staticRead(PRELUDE_FILE)
-proc compress_str(s : string, preset = COMPRESSION_PRESET): float64
-var PRELUDE_RATIO = compress_str("")
+const PRELUDE_STR {.exportc.} = staticRead(PRELUDE_FILE)
+proc compress_str(s : string, preset = COMPRESSION_PRESET): Future[float64] {.async.}
+var PRELUDE_RATIO = 0.0
 
 when defined(js):
   var console {.importc, nodecl.}: JsObject
   when not defined(extension):
     proc getLocalStorageItem(key : cstring) : JsObject {.importjs: "localStorage.getItem(#)".}
     proc setLocalStorageItem(key : cstring, val : cstring) {.importjs: "localStorage.setItem(#, #)".}
-  proc compress(str : cstring, mode : int) : seq[byte] {.importjs: "LZMA.compress(#, #)".}
-  console.log("Initialized with a prelude compression ratio of: " & $PRELUDE_RATIO)
+  proc lzma_compress(str : cstring, mode : int) : seq[byte] {.importjs: "LZMA.compress(#, #)".}
+  proc zlib_compress(str : cstring) : Future[seq[byte]] {.importjs: "ZLIB_compress(#)", async.}
 
+proc init() {.async.} =
+  PRELUDE_RATIO = await compress_str("")
+  when defined(js):
+    console.log("Initialized " & $ACTIVE_ENGINE & " with a prelude compression ratio of: " & $PRELUDE_RATIO)
+
+discard init()
 # Target independent wrapper for LZMA compression
-proc ti_compress(input : cstring, preset: int32, check: int32): seq[byte] = 
+proc ti_compress(input : cstring, preset: int32, check: int32): Future[seq[byte]] {.async.} = 
   when defined(c):
     return compress(input, preset, check)
-  when defined(js):
-    return compress(input, preset)
+  when not defined(c):
+    when ACTIVE_ENGINE == Engine.ZLibEngine:
+      return await zlib_compress(input)
+    when ACTIVE_ENGINE == Engine.LZMAEngine:
+      return lzma_compress(input, preset)
 
-proc compress_str(s : string, preset = COMPRESSION_PRESET): float64 =
+proc compress_str(s : string, preset = COMPRESSION_PRESET): Future[float64] {.async.} =
   when defined(c):
     let in_len = PRELUDE_STR.len + s.len
     var combined : string = PRELUDE_STR & s
@@ -50,12 +65,12 @@ proc compress_str(s : string, preset = COMPRESSION_PRESET): float64 =
       var combined : string = PRELUDE_STR & local_prelude & s
     let nonascii = newRegExp(r"[^\x00-\x7F]")
     combined = $combined.cstring.replace(nonascii, "")
-  let out_len = ti_compress(combined.cstring, preset, 0.int32).len
+  let out_len = (await ti_compress(combined.cstring, preset, 0.int32)).len
   return out_len.toFloat / in_len.toFloat
 
-proc score_string*(s : string, fuzziness : int): (string, float64) =
+proc score_string*(s : string, fuzziness : int): Future[(string, float64)] {.async.} =
   let 
-    sample_ratio = compress_str(s)
+    sample_ratio = await compress_str(s)
     delta = PRELUDE_RATIO - sample_ratio
   var determination = "AI"
   if delta < 0:
@@ -69,13 +84,13 @@ proc score_string*(s : string, fuzziness : int): (string, float64) =
   return (determination, abs(delta) * 100.0)
 
 when defined(c):
-  proc score_chunk(chunk : string, fuzziness : int): float64 =
-    var (d, s) = score_string(chunk, fuzziness)
+  proc score_chunk(chunk : string, fuzziness : int): Future[float64] {.async.} =
+    var (d, s) = await score_string(chunk, fuzziness)
     if d == "AI":
       return -1.0 * s
     return s
 
-proc run_on_text_chunked*(text : string, chunk_size : int = 1024, fuzziness : int = 3): (string, float64) =
+proc run_on_text_chunked*(text : string, chunk_size : int = 1024, fuzziness : int = 3): Future[(string, float64)] {.async.} =
   var inf : string = text
   when defined(c):
     inf = replace(inf, re" +", " ")
@@ -102,22 +117,8 @@ proc run_on_text_chunked*(text : string, chunk_size : int = 1024, fuzziness : in
 
   var scores : seq[(string, float64)] = @[]
 
-  when defined(c):
-    var flows : seq[FlowVar[float64]] = @[]
-    for c in chunks:
-      flows.add(spawn score_chunk(c, fuzziness))
-
-    for f in flows:
-      let score = ^f
-      var d : string = "Human"
-      if score < 0.0:
-        d = "AI"
-        scores.add((d, score * -1.0))
-      else:
-        scores.add((d, score))
-  when defined(js):
-    for c in chunks:
-      scores.add(score_string(c, fuzziness))
+  for c in chunks:
+    scores.add(await score_string(c, fuzziness))
 
   var ssum : float64 = 0.0
   for s in scores:
@@ -154,9 +155,11 @@ when defined(c) and isMainModule:
     quit 0
   for fn in filenames:
     if fileExists(fn):
+      proc print_results(score : Future[(string, float64)]) =
+        let (d, s) = score.read()
+        echo "(" & d & ", " & $s.formatFloat(ffDecimal, 8) & ")"
       echo fn
-      let (d, s) = run_on_text_chunked(readFile(fn))
-      echo "(" & d & ", " & $s.formatFloat(ffDecimal, 8) & ")"
+      run_on_text_chunked(readFile(fn)).addCallback(print_results)
 
 when defined(js) and isMainModule:
   when not defined(extension):
@@ -164,8 +167,7 @@ when defined(js) and isMainModule:
       document.getElementById("preset_value").value = ($COMPRESSION_PRESET).cstring
     if getLocalStorageItem("local_prelude") != jsNull:
       local_prelude = $(getLocalStorageItem("local_prelude").to(cstring))
-      PRELUDE_RATIO = compress_str("")
-      console.log("New prelude compression ratio of: " & $PRELUDE_RATIO)
+      discard init()
 
     proc add_to_lp() {.exportc.} =
       let new_text = document.getElementById("text_input").value
@@ -175,13 +177,12 @@ when defined(js) and isMainModule:
         setLocalStorageItem("local_prelude", existing & "\n" & new_text)
       else:
         setLocalStorageItem("local_prelude", new_text)
-      PRELUDE_RATIO = compress_str("")
-      console.log("New prelude compression ratio of: " & $PRELUDE_RATIO)
+      discard init()
 
-    proc do_detect() {.exportc.} =
+    proc do_detect() {.exportc, async.} =
       let
         text : string = $document.getElementById("text_input").value
-      var (d, s) = run_on_text_chunked(text)
+      var (d, s) = await run_on_text_chunked(text)
       var color = "rgba(255, 0, 0, " & $(s.round(3) * 10.0) & ")"
       if d == "Human":
         color = "rgba(0, 255, 0, " & $(s.round(3) * 10.0) & ")"
@@ -191,12 +192,11 @@ when defined(js) and isMainModule:
   when defined(extension):
     proc add_to_lp(text : cstring) {.exportc.} =
       local_prelude = local_prelude & "\n".cstring & text
-      PRELUDE_RATIO = compress_str("")
-      console.log("New prelude compression ratio of: " & $PRELUDE_RATIO)
+      discard init()
 
-    proc detect_string(s : cstring) : float {.exportc.} =
+    proc detect_string(s : cstring) : Future[float] {.exportc,async.} =
       # Returns the opacity for the element containing the passed string (higher for human-generated)
-      var (d, s) = run_on_text_chunked($s)
+      var (d, s) = await run_on_text_chunked($s)
       if d == "Human":
         return 1.0
       var opacity = 1.0 - s.round(3) * 10
