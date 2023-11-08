ecbf832e97a5c612b15651171acc39552da5cc8a
Jacob Torrey <jacob@thinkst.com>
2023-11-08 10:19:27 -0700

    Bump to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ZipPy setup file',
-    version='0.1.1',
+    version='0.1.2',
     packages=['zippy'],
     package_data={"": ["*.txt"]},
     entry_points={
